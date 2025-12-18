f5c80ca9781cde64aa5a079c6ff1bc7bf8979428
agno <asylkhan.yerzhanov@gmail.com>
2025-12-18 20:11:38 +0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,12 @@
-public class Main
-{
-    public static void main(String[] args) {
+public class Main {
+      public static void main(String[] args) {
         Bus bus1 = new Bus("777SSS", 84, 100, 55, "Ardaq Bakhyt", "On road");
         Bus bus2 = new Bus("777SSS", 84, 100, 55, "Ardaq Bakhyt", "On road");
-        Passenger pass1 = new Passenger("Zarina Daryn", 100011, 1, 84);
+        Passenger pass = new Passenger("Zarina Daryn", 100011, 1, 84);
         TransportService ts = new TransportService(84, "777SSS", 120);
 
         System.out.println(bus1);
-        System.out.println(pass1);
+        System.out.println(pass);
         System.out.println(ts);
 
         if (bus1.equals(bus2)) {
